b4f07e48d76c8182dc98d02ed2fdae5bb396615b
Ivan Mollov <ivan.xero.mollov@gmail.com>
2022-11-10 05:39:41 +0200

    Home page initial version ready

---
+++ src/components/Background.jsx
@@ -0,0 +1,7 @@
+import './Background.css'
+
+function Background(props) {
+   return <div className='background'>{props.children}</div>
+}
+
+export default Background
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Background from '../components/Background'
+
+function Home(props) {
+   return (
+      <Background>
+         <h1 style={{ color: 'blue' }}>ashdahshds</h1>
+      </Background>
+   )
+}
+
+export default Home

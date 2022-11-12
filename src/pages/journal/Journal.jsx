735e7151b68a5c99b83044992428eda9b0b6be39
Ivan Mollov <ivan.xero.mollov@gmail.com>
2022-11-12 02:44:05 +0200

    Styles journal page

--- src/pages/journal/Journal.jsx
+++ src/pages/journal/Journal.jsx
@@ -1,21 +1,25 @@
+import './Journal.css'
+
 import Log from './components/Log'
 
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData } from 'react-router-dom'
 
 export default function Journal(params) {
    const logs = useLoaderData()
 
    return (
-      <div className='Journal'>
-         {logs.map((log, logId) => {
-            return (
-               <Log
-                  date={log.date}
-                  title={log.title}
-                  key={logId}
-               />
-            )
-         })}
+      <div className='Journal centralizer'>
+         <div className='logs'>
+            {logs.map((log, logId) => {
+               return (
+                  <Log
+                     date={log.date}
+                     title={log.title}
+                     key={logId}
+                  />
+               )
+            })}
+         </div>
       </div>
    )
 }

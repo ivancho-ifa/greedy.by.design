d3baaef6044c45739c02151933a92bb2b52f5847
Ivan Mollov <ivan.xero.mollov@gmail.com>
2022-11-20 03:45:54 +0200

    Moves to CSS modules

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,58 +1,60 @@
-import './Navigation.css'
+import navigationStyles from './Navigation.module.css'
 
 import { Link, Outlet } from 'react-router-dom'
 
 export default function Navigation() {
    return (
-      <div className='rootLayout'>
-         <div className='page'>
+      <div className={`${navigationStyles.rootLayout}`}>
+         <div className={`${navigationStyles.page}`}>
             <Outlet />
          </div>
 
-         <nav className='Navigation row spreadChildren padded'>
-            <ul className='row'>
-               <li className='marginedSides'>
+         <nav
+            className={`${navigationStyles.Navigation} ${navigationStyles.row} ${navigationStyles.spreadChildren} ${navigationStyles.padded}}`}
+         >
+            <ul className={`${navigationStyles.row}`}>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/'>home</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/studio'>studio</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/projects'>projects</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/journal'>journal</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/shop'>shop</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/contact'>contact</Link>
                </li>
             </ul>
-            <ul className='row'>
-               <li className='smallMarginedSides'>
+            <ul className={`${navigationStyles.row}`}>
+               <li className={`${navigationStyles.smallMarginedSides}`}>
                   <a href='youtube.com'>
                      <img
-                        className='icon'
+                        className={`${navigationStyles.icon}`}
                         src='/youtube.svg'
                         alt='YouTube'
                      />
                   </a>
                </li>
-               <li className='smallMarginedSides'>
+               <li className={`${navigationStyles.smallMarginedSides}`}>
                   <a href='twitter.com'>
                      <img
-                        className='icon'
+                        className={`${navigationStyles.icon}`}
                         src='/twitter.svg'
                         alt='Twitter'
                      />
                   </a>
                </li>
-               <li className='smallMarginedSides'>
+               <li className={`${navigationStyles.smallMarginedSides}`}>
                   <a href='instagram.com'>
                      <img
-                        className='icon'
+                        className={`${navigationStyles.icon}`}
                         src='/instagram.svg'
                         alt='Instagram'
                      />

ae2e9b9e81af70cbf0458c5585dd5611d50c7e22
Ivan Mollov <ivan.xero.mollov@gmail.com>
2022-11-20 04:35:03 +0200

    Moves navigation to grid (#16)
    - Cleans navigation module up
    - Moves to CSS modules
    - Moves navigation to grid
    - Fixes small page creating a scrollbar
    - Optimizes to not use react effects for sizing the page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,68 +1,61 @@
-import './Navigation.css'
-
-import { Link, Outlet } from 'react-router-dom'
-import { Fragment, useRef } from 'react'
+import navigationStyles from './Navigation.module.css'
 import { useStyleFullscreenExcludingNavigation } from './utils/componentManipulation'
 
+import { Link, Outlet } from 'react-router-dom'
+
 export default function Navigation() {
-   const navigation = useRef()
-   const styleFullscreenExcludingNavigation = useStyleFullscreenExcludingNavigation(navigation)
-
-   console.log(`style: ${JSON.stringify(styleFullscreenExcludingNavigation)}`)
-
    return (
-      <Fragment>
-         <div style={styleFullscreenExcludingNavigation}>
+      <div id={`${navigationStyles.root}`}>
+         <div className={`${navigationStyles.page}`}>
             <Outlet />
          </div>
 
          <nav
-            ref={navigation}
-            className='Navigation row spreadChildren padded'
+            className={`${navigationStyles.Navigation} ${navigationStyles.row} ${navigationStyles.spreadChildren} ${navigationStyles.padded}}`}
          >
-            <ul className='row'>
-               <li className='marginedSides'>
+            <ul className={`${navigationStyles.row}`}>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/'>home</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/studio'>studio</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/projects'>projects</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/journal'>journal</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/shop'>shop</Link>
                </li>
-               <li className='marginedSides'>
+               <li className={`${navigationStyles.marginedSides}`}>
                   <Link to='/contact'>contact</Link>
                </li>
             </ul>
-            <ul className='row'>
-               <li className='smallMarginedSides'>
+            <ul className={`${navigationStyles.row}`}>
+               <li className={`${navigationStyles.smallMarginedSides}`}>
                   <a href='youtube.com'>
                      <img
-                        className='icon'
+                        className={`${navigationStyles.icon}`}
                         src='/youtube.svg'
                         alt='YouTube'
                      />
                   </a>
                </li>
-               <li className='smallMarginedSides'>
+               <li className={`${navigationStyles.smallMarginedSides}`}>
                   <a href='twitter.com'>
                      <img
-                        className='icon'
+                        className={`${navigationStyles.icon}`}
                         src='/twitter.svg'
                         alt='Twitter'
                      />
                   </a>
                </li>
-               <li className='smallMarginedSides'>
+               <li className={`${navigationStyles.smallMarginedSides}`}>
                   <a href='instagram.com'>
                      <img
-                        className='icon'
+                        className={`${navigationStyles.icon}`}
                         src='/instagram.svg'
                         alt='Instagram'
                      />
@@ -70,6 +63,6 @@ export default function Navigation() {
                </li>
             </ul>
          </nav>
-      </Fragment>
+      </div>
    )
 }

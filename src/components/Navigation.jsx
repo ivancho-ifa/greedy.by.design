0481c676d003b7e79d59d1a03580ffde699a4ace
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-01-22 20:14:51 +0200

    Fixes responsiveness (#20)
    * Fixes components width and ovrflow handling
    * Refactors index CSS
    * Makes contact page responsive
    * Moves home CSS to CSS module
    * Moves home CSS to grid
    * Makes home responsive
    * Adds padding removes font leading
    * Apply formatting changes
    * Fixes line height and bottom padding
    * Tweaks home style
    * Moves navigation to grid and makes it responsive
    * Hides navigation on mobile in hamburger menu
    * Apply formatting changes
    * Removes unused imports
    * Makes Journal page responsive and formats Navigation CSS
    * Fixes page resizing on hamburger menu toggle
    * Apply formatting changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,40 +1,68 @@
 import navigationStyles from './Navigation.module.css'
-import { useStyleFullscreenExcludingNavigation } from './utils/componentManipulation'
+import './hamburger-menu.css'
 
 import { Link, Outlet } from 'react-router-dom'
+import { useRef, useState, useEffect } from 'react'
 
 export default function Navigation() {
+   const navigation = useRef()
+   const hamburgerMenuToggle = useRef()
+   const [navigationHeight, setNavigationHeight] = useState(0)
+
+   useEffect(() => {
+      if (!hamburgerMenuToggle.current.checked) {
+         setNavigationHeight(navigation.current.clientHeight)
+      }
+   })
+
    return (
       <div id={`${navigationStyles.root}`}>
-         <div className={`${navigationStyles.page}`}>
+         <div
+            className={`${navigationStyles.page}`}
+            style={{ marginBottom: navigationHeight }}
+         >
             <Outlet />
          </div>
 
          <nav
             className={`${navigationStyles.Navigation} ${navigationStyles.row} ${navigationStyles.spreadChildren} ${navigationStyles.padded}}`}
+            ref={navigation}
+            style={{ position: 'fixed', bottom: 0 }}
          >
-            <ul className={`${navigationStyles.row}`}>
-               <li className={`${navigationStyles.marginedSides}`}>
+            <input
+               id={`${navigationStyles.menuToggle}`}
+               type='checkbox'
+               ref={hamburgerMenuToggle}
+            />
+            <label
+               class={`${navigationStyles.menuButtonContainer}`}
+               for={`${navigationStyles.menuToggle}`}
+            >
+               <div class={`${navigationStyles.menuButton}`}></div>
+            </label>
+
+            <ul className={`${navigationStyles.menu} ${navigationStyles.hamburgerMenu}`}>
+               <li className={`${navigationStyles.home}`}>
                   <Link to='/'>home</Link>
                </li>
-               <li className={`${navigationStyles.marginedSides}`}>
+               <li className={`${navigationStyles.studio}`}>
                   <Link to='/studio'>studio</Link>
                </li>
-               <li className={`${navigationStyles.marginedSides}`}>
+               <li className={`${navigationStyles.projects}`}>
                   <Link to='/projects'>projects</Link>
                </li>
-               <li className={`${navigationStyles.marginedSides}`}>
+               <li className={`${navigationStyles.journal}`}>
                   <Link to='/journal'>journal</Link>
                </li>
-               <li className={`${navigationStyles.marginedSides}`}>
+               <li className={`${navigationStyles.shop}`}>
                   <Link to='/shop'>shop</Link>
                </li>
-               <li className={`${navigationStyles.marginedSides}`}>
+               <li className={`${navigationStyles.contact}`}>
                   <Link to='/contact'>contact</Link>
                </li>
             </ul>
-            <ul className={`${navigationStyles.row}`}>
-               <li className={`${navigationStyles.smallMarginedSides}`}>
+            <ul className={`${navigationStyles.icons} ${navigationStyles.hamburgerMenu}`}>
+               <li className={`${navigationStyles.youtube}`}>
                   <a href='youtube.com'>
                      <img
                         className={`${navigationStyles.icon}`}
@@ -43,7 +71,7 @@ export default function Navigation() {
                      />
                   </a>
                </li>
-               <li className={`${navigationStyles.smallMarginedSides}`}>
+               <li className={`${navigationStyles.twitter}`}>
                   <a href='twitter.com'>
                      <img
                         className={`${navigationStyles.icon}`}
@@ -52,7 +80,7 @@ export default function Navigation() {
                      />
                   </a>
                </li>
-               <li className={`${navigationStyles.smallMarginedSides}`}>
+               <li className={`${navigationStyles.instagram}`}>
                   <a href='instagram.com'>
                      <img
                         className={`${navigationStyles.icon}`}

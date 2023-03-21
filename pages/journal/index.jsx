f500d8aa1720bfdfb24e8c6f5bb9a6d3fbe012c9
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-03-21 03:41:59 +0200

    Moves journal to a class component

--- pages/journal/index.jsx
+++ pages/journal/index.jsx
@@ -3,37 +3,44 @@ import journalStyles from 'styles/Journal.module.css'
 import LogThumbnail from 'components/LogThumbnail'
 import { getLogs } from 'utils/logs'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { withRouter } from 'next/router'
+import { Component } from 'react'
 
-export default function Journal({ logs }) {
-   const router = useRouter()
+class Journal extends Component {
+   render() {
+      return (
+         <div className={`${journalStyles.Journal} ${journalStyles.centralizer}`}>
+            <div className={`${journalStyles.logs}`}>
+               {this.props.logs.map((log, logId) => {
+                  return (
+                     <Link
+                        href={`${this.props.router.asPath}/${log._id}`}
+                        key={logId}
+                     >
+                        <LogThumbnail
+                           date={log.date}
+                           title={log.title}
+                           titleImage={log.titleImage}
+                           titleImageAlt={log.titleImageAlt}
+                        />
+                     </Link>
+                  )
+               })}
 
-   return (
-      <div className={`${journalStyles.Journal} ${journalStyles.centralizer}`}>
-         <div className={`${journalStyles.logs}`}>
-            {logs.map((log, logId) => {
-               return (
-                  <Link
-                     href={`${router.asPath}/${log._id}`}
-                     key={logId}
-                  >
-                     <LogThumbnail
-                        date={log.date}
-                        title={log.title}
-                        titleImage={log.titleImage}
-                        titleImageAlt={log.titleImageAlt}
-                     />
-                  </Link>
-               )
-            })}
+            </div>
          </div>
-      </div>
-   )
+      )
+   }
+
+   static getLayout(page) {
+      return <BottomNavigationLayout>{page}</BottomNavigationLayout>
+   }
 }
 
-Journal.getLayout = function (page) {
-   return <BottomNavigationLayout>{page}</BottomNavigationLayout>
-}
+const WithRouterWrapper = withRouter(Journal)
+WithRouterWrapper.getLayout = Journal.getLayout
+
+export default WithRouterWrapper
 
 export async function getStaticProps() {
    return {

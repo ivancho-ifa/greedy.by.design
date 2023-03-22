c84f24a689f9515fc3bb946590327a3fe2a8660c
Lint Action <lint-action@samuelmeuli.com>
2023-03-22 01:13:40 +0000

    Fix code style issues with Prettier

--- pages/api/rename-log.js
+++ pages/api/rename-log.js
@@ -2,16 +2,16 @@ import { changeLogId } from 'utils/logs'
 
 // TODO: Validate incoming URI
 export default async function handler(request, response) {
-    if (request.method !== 'PUT') {
-        return response.status(405).json({ error: `Method ${request.method} not allowed` })
-    }
+   if (request.method !== 'PUT') {
+      return response.status(405).json({ error: `Method ${request.method} not allowed` })
+   }
 
-    const { currentId, newId } = JSON.parse(request.body)
+   const { currentId, newId } = JSON.parse(request.body)
 
-    const renameError = await changeLogId(currentId, newId)
-    if (!renameError) {
-        return response.status(200).send()
-    } else {
-        return response.status(500).json({ error: renameError.error })
-    }
+   const renameError = await changeLogId(currentId, newId)
+   if (!renameError) {
+      return response.status(200).send()
+   } else {
+      return response.status(500).json({ error: renameError.error })
+   }
 }

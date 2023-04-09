73d385fb4e296bcd6acbdafaa86c246a1a28bb1b
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-04-10 02:22:40 +0300

    Trim unusable tools

--- components/editors/article/EditorTools.jsx
+++ components/editors/article/EditorTools.jsx
@@ -1,17 +1,13 @@
 import Paragraph from '@editorjs/paragraph'
 import Header from '@editorjs/header'
-import SimpleImage from '@editorjs/simple-image'
 import NestedList from '@editorjs/nested-list'
 import Quote from '@editorjs/quote'
 import Delimiter from '@editorjs/delimiter'
-import Embed from '@editorjs/embed'
 
 export const EDITOR_TOOLS = {
    paragraph: Paragraph,
    header: Header,
-   list: NestedList,
-   simpleImage: SimpleImage,
+   list: { class: NestedList, inlineToolbar: true },
    delimiter: Delimiter,
    quote: Quote,
-   embed: Embed,
 }

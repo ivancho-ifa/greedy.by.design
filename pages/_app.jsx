09ec9484aae7d71c5c74becabec9b5f8db5e86ec
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-04-30 23:52:27 +0300

    Enable analytics (#52)

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,17 @@
 import 'the-new-css-reset/css/reset.css'
 import 'styles/index.css'
 import 'styles/fonts.css'
+import { Analytics } from '@vercel/analytics/react'
+import { Fragment } from 'react'
 
 export default function MyApp({ Component, pageProps }) {
    // Use the layout defined at the page level, if available
    const getLayout = Component.getLayout || ((page) => page)
 
-   return getLayout(<Component {...pageProps} />)
+   return (
+      <Fragment>
+         {getLayout(<Component {...pageProps} />)}
+         <Analytics />
+      </Fragment>
+   )
 }

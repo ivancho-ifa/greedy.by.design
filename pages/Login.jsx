c57ac0b92d06660c8b4c584131def956da575078
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-05-05 01:45:41 +0300

    Add login by facebook

---
+++ pages/Login.jsx
@@ -0,0 +1,58 @@
+import BottomNavigationLayout from 'components/BottomNavigationLayout'
+import { useSession, signIn, signOut } from 'next-auth/react'
+import loginStyles from 'styles/Login.module.css'
+
+export default function Login() {
+   const { data: session, status } = useSession()
+
+   return (
+      <div className={`${loginStyles.Login}`}>
+         {!session && (
+            <div>
+               <div className={loginStyles.notSignedInText}>You are not signed in</div>
+               <div>
+                  <a
+                     href={`/api/auth/signin`}
+                     className={loginStyles.button}
+                     onClick={(e) => {
+                        e.preventDefault()
+                        signIn()
+                     }}
+                  >
+                     Sign in
+                  </a>
+               </div>
+            </div>
+         )}
+         {session?.user && (
+            <div>
+               {session.user.image && (
+                  <div
+                     style={{ backgroundImage: `url('${session.user.image}')` }}
+                     className={loginStyles.avatar}
+                  />
+               )}
+               <div className={loginStyles.signedInText}>
+                  <small>Signed in as</small>
+                  <br />
+                  <strong>{session.user.email ?? session.user.name}</strong>
+               </div>
+               <a
+                  href={`/api/auth/signout`}
+                  className={loginStyles.button}
+                  onClick={(e) => {
+                     e.preventDefault()
+                     signOut()
+                  }}
+               >
+                  Sign out
+               </a>
+            </div>
+         )}
+      </div>
+   )
+}
+
+Login.getLayout = function (page) {
+   return <BottomNavigationLayout>{page}</BottomNavigationLayout>
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,15 +3,18 @@ import 'styles/index.css'
 import 'styles/fonts.css'
 import { Analytics } from '@vercel/analytics/react'
 import { Fragment } from 'react'
+import { SessionProvider } from 'next-auth/react'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
    // Use the layout defined at the page level, if available
    const getLayout = Component.getLayout || ((page) => page)
 
    return (
       <Fragment>
-         {getLayout(<Component {...pageProps} />)}
-         <Analytics />
+         <SessionProvider>
+            {getLayout(<Component {...pageProps} />)}
+            <Analytics />
+         </SessionProvider>
       </Fragment>
    )
 }

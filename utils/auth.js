4bb4aaae3de4eddf9586df587cb49a2313cca530
Lint Action <lint-action@samuelmeuli.com>
2023-05-23 00:23:19 +0000

    Fix code style issues with Prettier

--- utils/auth.js
+++ utils/auth.js
@@ -3,17 +3,17 @@ import { useSession } from 'next-auth/react'
 const admins = JSON.parse(process.env.NEXT_PUBLIC_ADMINS)
 
 export function isAdmin(session) {
-    return session && admins.includes(session.user.email)
+   return session && admins.includes(session.user.email)
 }
 
 // eslint-disable-next-line react/display-name
 export const withSession = (Component) => (props) => {
-    const session = useSession()
+   const session = useSession()
 
-    return (
-        <Component
-            session={session}
-            {...props}
-        />
-    )
+   return (
+      <Component
+         session={session}
+         {...props}
+      />
+   )
 }

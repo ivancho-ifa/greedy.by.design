d194226e3deae7ab8dde9aadef15d56d6a9ca3e2
Ivan Mollov <ivan.xero.mollov@gmail.com>
2023-07-08 01:09:20 +0300

    Set-up PostCSS plug-ins related to Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    // postcss-import needs to be first plugin
+    'postcss-import': {},
     autoprefixer: {},
+    tailwindcss: {},
+    'tailwindcss/nesting': {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
   },
 }
